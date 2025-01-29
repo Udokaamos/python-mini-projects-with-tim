cf235115f31d0dced5025db19acd3d0cf08351f7
Udokaamos <udokaamos@gmail.com>
2025-01-29 14:26:16 +0100

    included players

--- project1.py
+++ project1.py
@@ -8,5 +8,16 @@ def roll():
 
     return roll
 
-value = roll()
-print(value)
+"""Including Players to the game using While loop"""
+while True:
+    players = input("Enter the number of players(2 - 4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between 2 - 4 players")
+    else:
+        print("Invalid, try again.")
+
+print(players)
