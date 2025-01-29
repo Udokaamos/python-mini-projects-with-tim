c6a3f9a5afed7f6ccbde1f6e20fd753c621534a5
Udokaamos <udokaamos@gmail.com>
2025-01-29 14:11:23 +0100

    how to generate a random roll

---
+++ project1.py
@@ -0,0 +1,12 @@
+import random
+
+"""The Pig Game"""
+def roll():
+    min_value = 1
+    max_value = 6
+    roll = random.randint(min_value, max_value)
+
+    return roll
+
+value = roll()
+print(value)
